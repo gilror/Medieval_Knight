43852584b718c36021083f061a7e56af4ce367a7
Gilberto Rodriguez <gilror1090@gmail.com>
2022-10-24 23:54:11 -0500

    Combate basico, se preparo al enemigo

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,12 +1,30 @@
-using System.Security.Cryptography;
+using System;
 using RPG.Movement;
 using UnityEngine;
+using RPG.Combat;
 
 namespace RPG.Control{
     public class PlayerController : MonoBehaviour {
 
     private void Update() {
-         if (Input.GetMouseButton(0))
+         InteractWithCombat();
+         InteractWithMovement();
+    }
+
+    private void InteractWithCombat(){
+       RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+       foreach(RaycastHit hit in hits){
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            if (target == null) continue;
+
+            if (Input.GetMouseButtonDown(0)){
+                GetComponent<Fighter>().Attack(target);
+            }
+       }
+    }
+
+    private void InteractWithMovement(){
+        if (Input.GetMouseButton(0))
          {
              MoveToCursor();
          }
@@ -14,13 +32,16 @@ namespace RPG.Control{
 
     private void MoveToCursor()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
+        bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
         if (hasHit)
         {
             GetComponent<Mover>().MoveTo(hit.point);
         }
+    }
+
+    private static Ray GetMouseRay(){
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 }
 }
